d638dcf6c1dc82c02246d409aa5ef26627bf364d
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-07-27 16:08:31 -0300

    Change to allow cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -12,6 +13,7 @@ var apiRouter = require('./routes/api');
 
 var app = express();
 
+const cors = require('cors'); 
 //mongoose setup
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
@@ -26,11 +28,13 @@ async function main() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
