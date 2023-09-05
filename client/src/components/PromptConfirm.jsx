1c05ea71ddc7bec52046ff0f87003186bbe7bc57
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2023-09-05 16:36:00 -0300

    Component for confirm prompts

---
+++ client/src/components/PromptConfirm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./Prompts.css";
+
+export default function PromptConfirm({
+	message,
+	onConfirm,
+	onCancel,
+	hidePrompt,
+}) {
+	const [userSayYes, setUserSayYes] = useState(null);
+
+	const handleYesClick = () => {
+		setUserSayYes(true);
+		if (onConfirm && typeof onConfirm === 'function') {
+			onConfirm();
+		}
+		hidePrompt(false);
+	};
+
+	const handleNoClick = () => {
+		setUserSayYes(false);
+		if (onCancel && typeof onCancel === 'function') {
+			onCancel(false);
+		}
+		hidePrompt(false);
+	};
+	return (
+		<div className="prompt-confirm">
+			<p>{message}</p>
+			<button onClick={handleYesClick} className="prompt-confirm__button">Yes</button>
+			<button onClick={handleNoClick} className="prompt-confirm__button prompt-confirm__button--cancel">No</button>
+		</div>
+	);
+}
