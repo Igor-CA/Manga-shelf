e0211d6cc131bb28acbd507e6ea482776283d29e
Aquele_Cara <122459409+Igor-CA@users.noreply.github.com>
2025-07-24 16:07:24 -0300

    Fix graphs UI problems

--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,10 +1,4 @@
-import {
-	BarChart,
-	Bar,
-	Tooltip,
-	ResponsiveContainer,
-	XAxis,
-} from "recharts";
+import { BarChart, Bar, Tooltip, ResponsiveContainer, XAxis } from "recharts";
 const CustomTooltip = ({ active, payload, label, total }) => {
 	if (active && payload && payload.length) {
 		return (
@@ -12,7 +6,8 @@ const CustomTooltip = ({ active, payload, label, total }) => {
 				<p>{`Gênero: ${label}`}</p>
 				<p>{`Quantidade: ${payload[0].value} `}</p>
 				<p>
-					{Math.round((payload[0].value / total) * 100)}% das suas obras tem traços de {label}
+					{Math.round((payload[0].value / total) * 100)}% das suas obras tem
+					traços de {label}
 				</p>
 			</div>
 		);
@@ -34,43 +29,53 @@ const CustomCursor = ({ x, y, width, height }) => {
 };
 
 export default function BarChartComponent({ chartTitle, data, total }) {
-
 	return (
 		<div className="chart-container chart-container--grow">
 			<div style={{ flexGrow: 1 }}>
 				<p className="chart__title">{chartTitle}</p>
 				<div className="pie-chart_container">
-					<ResponsiveContainer width="100%" height="100%">
-						<BarChart data={data}>
-							<Bar
-								dataKey="count"
-                                className="bar-chart__bar"
-								isAnimationActive={false}
-								radius={[5, 5, 0, 0]}
-							></Bar>
-							<XAxis
-								dataKey="name"
-								tick={(props) => {
-									const { verticalAnchor, visibleTicksCount, tickFormatter, ...restProps } = props;
-									return (
-										<text
-											{...restProps}
-											className="bar-chart__x_axis"
-											textAnchor="middle"
-											dominantBaseline="central"
-											y={props.y + 5} // Fine-tune vertical position
-										>
-											{props.payload.value}
-										</text>
-									);
-								}}
-							/>
-							<Tooltip
-								cursor={<CustomCursor />}
-								content={<CustomTooltip total={total} />}
-							/>{" "}
-						</BarChart>
-					</ResponsiveContainer>
+					{(data.length > 0) ? (
+						<ResponsiveContainer width="100%" height="100%">
+							<BarChart data={data}>
+								<Bar
+									dataKey="count"
+									className="bar-chart__bar"
+									isAnimationActive={false}
+									radius={[5, 5, 0, 0]}
+								></Bar>
+								<XAxis
+									dataKey="name"
+									tick={(props) => {
+										const {
+											verticalAnchor,
+											visibleTicksCount,
+											tickFormatter,
+											...restProps
+										} = props;
+										return (
+											<text
+												{...restProps}
+												className="bar-chart__x_axis"
+												textAnchor="middle"
+												dominantBaseline="central"
+												y={props.y + 5} // Fine-tune vertical position
+											>
+												{props.payload.value}
+											</text>
+										);
+									}}
+								/>
+								<Tooltip
+									cursor={<CustomCursor />}
+									content={<CustomTooltip total={total} />}
+								/>{" "}
+							</BarChart>
+						</ResponsiveContainer>
+					):
+					<div className="empty-graph-message-container">
+						<p className="empty-graph-message">Sem informações o suficiente para calcular esse grafico. Adicione mais obras e volte mais tarde</p>
+					</div>
+					}
 				</div>
 			</div>
 		</div>
--- client/src/components/PieChart.jsx
+++ client/src/components/PieChart.jsx
@@ -16,50 +16,60 @@ const MAX_SHOW = 4;
 export default function PieChartComponent({ chartTitle, data, total }) {
 	const getPercentageString = (count, total) => {
 		const percentage = Math.round((count / total) * 100);
-		if (percentage < 1){
-			return "< 1"
+		if (percentage < 1) {
+			return "< 1";
 		}
-		return percentage
+		return percentage;
 	};
 	return (
 		<div className="chart-container">
 			<div>
 				<p className="chart__title">{chartTitle}</p>
 				<div className="pie-chart_container">
-					<ResponsiveContainer width="100%" height="80%">
-						<PieChart width="100%" height="100%">
-							<Pie
-								data={(() => {
-									if (data.length <= MAX_SHOW) {
-										return data;
-									}
-									const otherVal = data
-										.slice(MAX_SHOW)
-										.reduce((sum, value) => sum + value.count, 0);
-									const list = [
-										...data.slice(0, MAX_SHOW),
-										{ count: otherVal, name: "Outros" },
-									];
-									return list;
-								})()}
-								cx="50%"
-								cy="50%"
-								labelLine={false} // No label line for this layer
-								outerRadius={100}
-								fill="#8884d8"
-								dataKey="count"
-							>
-								{/* Render count at the center */}
-								{data.map((entry, index) => (
-									<Cell
-										key={`cell-${index}`}
-										fill={COLORS[index % COLORS.length]}
-									/>
-								))}
-							</Pie>
-							<Tooltip />
-						</PieChart>
-					</ResponsiveContainer>
+					{data.length > 0 ? (
+						<ResponsiveContainer width="100%" height="80%">
+							<PieChart width="100%" height="100%">
+								<Pie
+									data={(() => {
+										if (data.length <= MAX_SHOW) {
+											return data;
+										}
+										const otherVal = data
+											.slice(MAX_SHOW)
+											.reduce((sum, value) => sum + value.count, 0);
+										const list = [
+											...data.slice(0, MAX_SHOW),
+											{ count: otherVal, name: "Outros" },
+										];
+										return list;
+									})()}
+									cx="50%"
+									cy="50%"
+									labelLine={false} // No label line for this layer
+									outerRadius={100}
+									fill="#8884d8"
+									dataKey="count"
+									stroke={data.length > 1 ? "var(--foreground)" : "none"} // Use a white stroke only when there's more than one slice
+								>
+									{/* Render count at the center */}
+									{data.map((entry, index) => (
+										<Cell
+											key={`cell-${index}`}
+											fill={COLORS[index % COLORS.length]}
+										/>
+									))}
+								</Pie>
+								<Tooltip />
+							</PieChart>
+						</ResponsiveContainer>
+					) : (
+						<div className="empty-graph-message-container">
+							<p className="empty-graph-message">
+								Sem informações o suficiente para calcular esse grafico.
+								Adicione mais obras e volte mais tarde
+							</p>
+						</div>
+					)}
 				</div>
 			</div>
 			<ol className="pie-chart__subtitle-container">
