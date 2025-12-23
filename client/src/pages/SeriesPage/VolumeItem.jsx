10e620f9b5a4cd3d99bb25db211fe0e98c168a98
Igor-CA <igorcaldeira.andrade@gmail.com>
2025-12-23 17:07:17 -0300

    lazy loaded images on volumes list

--- client/src/pages/SeriesPage/VolumeItem.jsx
+++ client/src/pages/SeriesPage/VolumeItem.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function VolumeItem({
@@ -6,6 +7,7 @@ export default function VolumeItem({
 	handleChange,
 	user,
 }) {
+	const [loaded, setLoaded] = useState(false);
 	const { volumeId, image, volumeNumber } = volumeInfo;
 	const ownsVolume = localVolumeState
 		? localVolumeState.find((element) => element.volumeId === volumeId)
@@ -14,6 +16,9 @@ export default function VolumeItem({
 
 	const handleCheckboxChange = (e) => {
 		handleChange(e, volumeId);
+	};
+	const handleLoading = () => {
+		setLoaded(true);
 	};
 	return (
 		<div key={volumeId} className="series__volume-item">
@@ -26,30 +31,37 @@ export default function VolumeItem({
 				checked={Math.random() < 0.5}
 				onChange={handleCheckboxChange}
 			/>
-			<div to={`/volume/${volumeId}`} className="series__volume__image-wrapper">
-				<Link to={`/volume/${volumeId}`}>
-					<img
-						src={`${
-							import.meta.env.REACT_APP_HOST_ORIGIN
-						}/images/medium/${image}`}
-						srcSet={`
+			<Link
+				to={`/volume/${volumeId}`}
+				className="series__volume__image-wrapper"
+			>
+				<img
+					src={`${
+						import.meta.env.REACT_APP_HOST_ORIGIN
+					}/images/medium/${image}`}
+					srcSet={`
 						${import.meta.env.REACT_APP_HOST_ORIGIN}/images/small/${image} 100w,
 						${import.meta.env.REACT_APP_HOST_ORIGIN}/images/medium/${image} 400w, 
 						${import.meta.env.REACT_APP_HOST_ORIGIN}/images/large/${image} 700w,
 						${import.meta.env.REACT_APP_HOST_ORIGIN}/images/extralarge/${image} 1000w,`}
-						sizes=" (min-width: 1024px) 15vw,
+					sizes=" (min-width: 1024px) 15vw,
 							(min-width: 768px) 20vw, 
 							(max-width: 768px) 20vw, "
-						loading="lazy"
-						alt={`cover volume ${volumeNumber}`}
-						className="series__volume__image"
-					/>
-				</Link>
-			</div>
+					loading="lazy"
+					alt={`cover volume ${volumeNumber}`}
+					className={`series__volume__image ${
+						!loaded && "series__volume__image--loading"
+					}`}
+					onLoad={handleLoading}
+				/>
+			</Link>
 			<div className="series__volume__body">
 				<strong className="checkmark-label">Volume {volumeNumber}</strong>
 
-				<label htmlFor={`have-volume-check-mark-${volumeId}`} className="action-label">
+				<label
+					htmlFor={`have-volume-check-mark-${volumeId}`}
+					className="action-label"
+				>
 					<span className="text-add">Adicionar</span>
 					<span className="text-owned">Remover</span>
 				</label>
