9baff84b4ce57ef8b5d29812a73587cae7fffb92
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-01-12 15:05:33 -0300

    changed profile navbar to a separate component

---
+++ client/src/components/ContentNavbar.jsx
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import "./ContentNavbar.css"; 
+
+export default function ContentNavbar({ links }) {
+  return (
+    <div className="content-navbar">
+      <nav className="container">
+        <ul className="content-navbar__list">
+          {links.map((link) => (
+            <li key={link.to}>
+              <NavLink
+                to={link.to}
+                end={link.end} 
+                className={({ isActive }) =>
+                  isActive
+                    ? "content-navbar__link content-navbar__link--active"
+                    : "content-navbar__link"
+                }
+              >
+                {link.label}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
