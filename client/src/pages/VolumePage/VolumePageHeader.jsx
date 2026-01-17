f314296a958e37d1d67cdd2ba39e7ef87a621cc8
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-01-17 16:50:36 -0300

    Refactored series and volumes page to use same base component

--- client/src/pages/VolumePage/VolumePageHeader.jsx
+++ client/src/pages/VolumePage/VolumePageHeader.jsx
@@ -1,44 +1,35 @@
-import { useContext, useRef, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import { UserContext } from "../../contexts/userProvider";
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { UserContext } from "../../components/userProvider";
 import axios from "axios";
-import SkeletonHeader from "../../components/SkeletonPage";
+import { getOwnedVolumeInfo } from "../SeriesPage/utils";
+import { useEditVolume } from "../../components/EditVolumeContext";
+import { messageContext } from "../../components/messageStateProvider";
+import ContentHeader from "../../components/contentHeader/contentHeader";
 
-import "../SeriesPage/SeriesPage.css";
-import ActionDropdown from "../SeriesPage/ActionsDropdown";
-import { getOwnedVolumeInfo, printArray } from "../SeriesPage/utils";
-import { useEditVolume } from "../../contexts/EditVolumeContext";
-import { messageContext } from "../../contexts/messageStateProvider";
 export default function VolumeInfoCard({ volumeData }) {
 	const { id } = useParams();
-	const seriesSummarry = useRef(null);
-	const [showingMore, setShowingMore] = useState(false);
 	const navigate = useNavigate();
 	const { openEditModal } = useEditVolume();
 	const { addMessage } = useContext(messageContext);
-
 	const { user, setOutdated } = useContext(UserContext);
-	const [loaded, setLoaded] = useState(false);
 
-	const handleLoading = () => {
-		setLoaded(true);
-	};
 	const checkOwnedVolume = () => {
 		return user?.ownedVolumes
 			? user.ownedVolumes.some(
-					(entry) => entry.volume.toString() === id.toString()
-			  )
+					(entry) => entry.volume.toString() === id.toString(),
+				)
 			: false;
 	};
 
 	const addOrRemoveVolume = async (isAdding) => {
 		try {
 			if (!volumeData) return;
-
 			const url = isAdding
 				? `${import.meta.env.REACT_APP_HOST_ORIGIN}/api/user/add-volume`
 				: `${import.meta.env.REACT_APP_HOST_ORIGIN}/api/user/remove-volume`;
 			const amountVolumesFromSeries = volumeData.serie.volumes.length;
+
 			await axios({
 				method: "POST",
 				data: {
@@ -91,115 +82,22 @@ export default function VolumeInfoCard({ volumeData }) {
 		isRed: user && checkOwnedVolume(),
 		onClick: handleChange,
 	};
-	return volumeData ? (
-		<header className="content-header">
-			<div className="header__bg-image-container">
-				<div
-					style={{
-						backgroundImage: `url('${
-							import.meta.env.REACT_APP_HOST_ORIGIN
-						}/images/medium/${volumeData.image}')`,
-					}}
-					className="header__bg-image"
-					role="img"
-					aria-label="background-image"
-				></div>
-				<div className="banner-shade"></div>
-			</div>
-			<div className="container">
-				<div className="header-container">
-					<div className="header__art-container">
-						<div className="header__cover-image-container">
-							<img
-								src={`${import.meta.env.REACT_APP_HOST_ORIGIN}/images/medium/${
-									volumeData.image
-								}`}
-								srcSet={
-									`${import.meta.env.REACT_APP_HOST_ORIGIN}/images/small/${
-										volumeData.image
-									} 100w,` +
-									`${import.meta.env.REACT_APP_HOST_ORIGIN}/images/medium/${
-										volumeData.image
-									} 400w,` +
-									`${import.meta.env.REACT_APP_HOST_ORIGIN}/images/large/${
-										volumeData.image
-									} 700w,` +
-									`${import.meta.env.REACT_APP_HOST_ORIGIN}/images/extralarge/${
-										volumeData.image
-									} 1000w,`
-								}
-								sizes=" (min-width: 768px) 360px, 
-                                            (max-width: 768px) 100vw,"
-								loading="lazy"
-								alt={`cover ${volumeData.serie?.title} volume ${volumeData.number}`}
-								className={`header__cover-image ${
-									!loaded && "header__cover-image--loading"
-								}`}
-								onLoad={handleLoading}
-							/>
-							{volumeData?.serie?.isAdult && (
-								<div className="series-card__adult-indicator">+18</div>
-							)}
-						</div>
-						<ActionDropdown
-							mainAction={mainAction}
-							options={dropdownOptions}
-							isDisabled={!user}
-						/>
-					</div>
-					<div className="header__main-info-container">
-						<div className="header__title-container">
-							<div>
-								<h1 className="content-title">{volumeData.serie?.title}</h1>
-								<h2>Volume {volumeData.number}</h2>
-							</div>
-							{volumeData.serie?.authors?.length > 0 && (
-								<span className="content-author">
-									Obra de: {volumeData.serie?.authors && printArray(volumeData.serie?.authors)}
-								</span>
-							)}
-						</div>
-						<div className="header__secondary-info">
-							<ul className="header__genres-list">
-								{volumeData.serie?.genres.map((genre, index) => {
-									return (
-										<li className="header__genre-tag" key={index}>
-											{genre}
-										</li>
-									);
-								})}
-							</ul>
-						</div>
-					</div>
-				</div>
 
-				{volumeData.summary?.length > 0 && (
-					<div className="content__details-summary">
-						<strong>Sinopse:</strong>
-						<div
-							ref={seriesSummarry}
-							className={`content__summary ${
-								showingMore ? "content__summary--show-full" : ""
-							}`}
-						>
-							{volumeData.summary.map((paragraph, i) => (
-								<p key={i}>{paragraph}</p>
-							))}
-						</div>
-						{!showingMore && (
-							<button
-								className="show-more__button"
-								aria-label="Mostrar mais"
-								onClick={() => setShowingMore(true)}
-							>
-								Mostrar mais
-							</button>
-						)}
-					</div>
-				)}
-			</div>
-		</header>
-	) : (
-		<SkeletonHeader></SkeletonHeader>
+	return (
+		<ContentHeader
+			data={volumeData}
+			imageFilename={volumeData?.image}
+			backgroundImageUrl={`${
+				import.meta.env.REACT_APP_HOST_ORIGIN
+			}/images/medium/${volumeData?.image}`}
+			title={volumeData?.serie?.title}
+			subtitle={`Volume ${volumeData?.number}`}
+			authors={volumeData?.serie?.authors}
+			genres={volumeData?.serie?.genres}
+			isAdult={volumeData?.serie?.isAdult}
+			summary={volumeData?.summary}
+			actions={{ mainAction, dropdownOptions, isDisabled: !user }}
+			// navLinks={navLinks}
+		/>
 	);
 }
