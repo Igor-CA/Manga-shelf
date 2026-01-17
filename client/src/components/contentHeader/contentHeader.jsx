741ebc559049c2bc06949695e8b45e4390b10551
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-01-17 16:50:36 -0300

    Refactored series and volumes page to use same base component

---
+++ client/src/components/contentHeader/contentHeader.jsx
@@ -0,0 +1,146 @@
+import { useEffect, useRef, useState } from "react";
+import ActionDropdown from "../customInputs/ActionsDropdown";
+import "../../pages/SeriesPage/SeriesPage.css";
+import ContentNavbar from "../navbars/ContentNavbar";
+import SkeletonHeader from "../SkeletonHeader";
+import { printArray } from "../../pages/SeriesPage/utils";
+
+export default function ContentHeader({
+	data,
+	imageFilename,
+	backgroundImageUrl,
+	title,
+	subtitle,
+	authors,
+	genres,
+	isAdult,
+	summary,
+	actions,
+	navLinks,
+}) {
+	const [showingMore, setShowingMore] = useState(false);
+	const [loaded, setLoaded] = useState(false);
+	const summaryRef = useRef(null);
+
+	useEffect(() => {
+		if (summaryRef.current) {
+			setShowingMore(
+				summaryRef.current.scrollHeight <= summaryRef.current.clientHeight,
+			);
+		}
+	}, [data]);
+
+	const handleLoading = () => {
+		setLoaded(true);
+	};
+
+	if (!data) return <SkeletonHeader />;
+
+	return (
+		<header className="content-header">
+			<div className="header__bg-image-container">
+				<div
+					style={{
+						backgroundImage: `url('${backgroundImageUrl}')`,
+					}}
+					className="header__bg-image"
+					role="img"
+					aria-label="background-image"
+				></div>
+				<div className="banner-shade"></div>
+			</div>
+			<div className="container">
+				<div className="header-container">
+					<div className="header__art-container">
+						<div className="header__cover-image-container">
+							<img
+								src={`${
+									import.meta.env.REACT_APP_HOST_ORIGIN
+								}/images/medium/${imageFilename}`}
+								srcSet={
+									`${
+										import.meta.env.REACT_APP_HOST_ORIGIN
+									}/images/small/${imageFilename} 100w,` +
+									`${
+										import.meta.env.REACT_APP_HOST_ORIGIN
+									}/images/medium/${imageFilename} 400w,` +
+									`${
+										import.meta.env.REACT_APP_HOST_ORIGIN
+									}/images/large/${imageFilename} 700w,` +
+									`${
+										import.meta.env.REACT_APP_HOST_ORIGIN
+									}/images/extralarge/${imageFilename} 1000w,`
+								}
+								sizes=" (min-width: 768px) 360px, (max-width: 768px) 100vw,"
+								loading="lazy"
+								alt={`cover ${title}`}
+								className={`header__cover-image ${
+									!loaded && "header__cover-image--loading"
+								}`}
+								onLoad={handleLoading}
+							/>
+							{isAdult && (
+								<div className="series-card__adult-indicator">+18</div>
+							)}
+						</div>
+						<ActionDropdown
+							mainAction={actions.mainAction}
+							options={actions.dropdownOptions}
+							isDisabled={actions.isDisabled}
+						/>
+					</div>
+					<div className="header__main-info-container">
+						<div className="header__title-container">
+							<div>
+								<h1 className="content-title">{title}</h1>
+								{subtitle && <h2>{subtitle}</h2>}
+							</div>
+							{authors?.length > 0 && (
+								<span className="content-author">
+									Obra de: {printArray(authors)}
+								</span>
+							)}
+						</div>
+						<div className="header__secondary-info">
+							<ul className="header__genres-list">
+								{genres?.map((genre, index) => {
+									return (
+										<li className="header__genre-tag" key={index}>
+											{genre}
+										</li>
+									);
+								})}
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				{summary?.length > 0 && (
+					<div className="content__details-summary">
+						<strong>Sinopse:</strong>
+						<div
+							ref={summaryRef}
+							className={`content__summary ${
+								showingMore ? "content__summary--show-full" : ""
+							}`}
+						>
+							{summary.map((paragraph, i) => (
+								<p key={i}>{paragraph}</p>
+							))}
+						</div>
+						{!showingMore && (
+							<button
+								className="show-more__button"
+								aria-label="Mostrar mais"
+								onClick={() => setShowingMore(true)}
+							>
+								Mostrar mais
+							</button>
+						)}
+					</div>
+				)}
+			</div>
+			{navLinks && <ContentNavbar links={navLinks} />}
+		</header>
+	);
+}
--- client/src/pages/SeriesPage/SeriesPageHeader.jsx
+++ client/src/pages/SeriesPage/SeriesPageHeader.jsx
@@ -1,21 +1,14 @@
-import { useContext, useEffect, useRef, useState, useMemo } from "react";
-import ActionDropdown from "./ActionsDropdown";
-import "./SeriesPage.css";
+import { useContext, useMemo } from "react";
+import { UserContext } from "../../contexts/userProvider";
 import {
 	checkIfInWishlist,
 	getCompletionPercentage,
 	getSeriesStatus,
-	printArray,
 } from "./utils";
-import { UserContext } from "../../contexts/userProvider";
-import SkeletonHeader from "../../components/SkeletonHeader";
-import ContentNavbar from "../../components/navbars/ContentNavbar";
+import ContentHeader from "../../components/contentHeader/contentHeader";
 
 export default function SeriesPageHeader({ seriesInfo, actions }) {
 	const { user } = useContext(UserContext);
-	const [showingMore, setShowingMore] = useState(false);
-	const [loaded, setLoaded] = useState(false);
-	const seriesSummarry = useRef(null);
 	const {
 		handleSelectAllVolumes,
 		toggleSeriesInList,
@@ -23,17 +16,21 @@ export default function SeriesPageHeader({ seriesInfo, actions }) {
 		toggleDrop,
 	} = actions;
 
-	useEffect(() => {
-		setShowingMore(
-			seriesSummarry.current?.scrollHeight <=
-				seriesSummarry.current?.clientHeight
-		);
-	}, [seriesInfo]);
+	const { seriesCover, title, summary, genres, authors, id, isAdult } =
+		seriesInfo || {};
 
-	const handleLoading = () => {
-		setLoaded(true);
+	const isSeriesInUserList =
+		user?.userList?.some(
+			(seriesObj) =>
+				seriesObj.Series._id.toString() === id || seriesObj.Series === id,
+		) ?? false;
+
+	const mainAction = {
+		label: !isSeriesInUserList ? "Adicionar coleção" : "Remover coleção",
+		isRed: isSeriesInUserList,
+		onClick: () => toggleSeriesInList(!isSeriesInUserList),
 	};
-	const { seriesCover, title, summary, genres, authors, id } = seriesInfo || {};
+
 	const dropdownOptions = [
 		{
 			label:
@@ -63,133 +60,31 @@ export default function SeriesPageHeader({ seriesInfo, actions }) {
 
 	const navLinks = useMemo(
 		() => [
-			{ to: `/series/${seriesInfo?.id}`, label: "Geral", end: true },
-			{ to: `/series/${seriesInfo?.id}/volumes`, label: "Volumes" },
-			{ to: `/series/${seriesInfo?.id}/related`, label: "Obras Relacionadas" },
+			{ to: `/series/${id}`, label: "Geral", end: true },
+			{ to: `/series/${id}/volumes`, label: "Volumes" },
+			{ to: `/series/${id}/related`, label: "Obras Relacionadas" },
 			/*
-			{ to: `/series/${seriesInfo?.id}/reviews`, label: "Reviews" },
-			{ to: `/series/${seriesInfo?.id}/user-volumes`, label: "Seus volumes" }, //Conditioned rendered
+			{ to: `/series/${id}/reviews`, label: "Reviews" },
+			{ to: `/series/${id}/user-volumes`, label: "Seus volumes" }, //Conditioned rendered
 			*/
 		],
-		[seriesInfo]
+		[id],
 	);
-	const isSeriesInUserList =
-		user?.userList?.some(
-			(seriesObj) =>
-				seriesObj.Series._id.toString() === id || seriesObj.Series === id
-		) ?? false;
-	const mainAction = {
-		label: !isSeriesInUserList ? "Adicionar coleção" : "Remover coleção",
-		isRed: isSeriesInUserList,
-		onClick: () => toggleSeriesInList(!isSeriesInUserList),
-	};
-	return seriesInfo ? (
-		<header className="content-header">
-			<div className="header__bg-image-container">
-				<div
-					style={{
-						backgroundImage: `url("${
-							import.meta.env.REACT_APP_HOST_ORIGIN
-						}/images/medium/${seriesCover}")`,
-					}}
-					className="header__bg-image"
-					role="img"
-					aria-label="background-image"
-				></div>
-				<div className="banner-shade"></div>
-			</div>
-			<div className="container">
-				<div className="header-container">
-					<div className="header__art-container">
-						<div className="header__cover-image-container">
-							<img
-								src={`${
-									import.meta.env.REACT_APP_HOST_ORIGIN
-								}/images/medium/${seriesCover}`}
-								srcSet={
-									`${
-										import.meta.env.REACT_APP_HOST_ORIGIN
-									}/images/small/${seriesCover} 100w,` +
-									`${
-										import.meta.env.REACT_APP_HOST_ORIGIN
-									}/images/medium/${seriesCover} 400w,` +
-									`${
-										import.meta.env.REACT_APP_HOST_ORIGIN
-									}/images/large/${seriesCover} 700w,` +
-									`${
-										import.meta.env.REACT_APP_HOST_ORIGIN
-									}/images/extralarge/${seriesCover} 1000w,`
-								}
-								sizes=" (min-width: 768px) 360px, 
-                                            (max-width: 768px) 100vw,"
-								loading="lazy"
-								alt={`cover volume ${title}`}
-								className={`header__cover-image ${
-									!loaded && "header__cover-image--loading"
-								}`}
-								onLoad={handleLoading}
-							/>
-							{seriesInfo.isAdult && (
-								<div className="series-card__adult-indicator">+18</div>
-							)}
-						</div>
-						<ActionDropdown
-							mainAction={mainAction}
-							options={dropdownOptions}
-							isDisabled={!user}
-						/>
-					</div>
-					<div className="header__main-info-container">
-						<div className="header__title-container">
-							<h1 className="content-title">{title}</h1>
-							{authors?.length > 0 && (
-								<span className="content-author">
-									Obra de: {authors && printArray(authors)}
-								</span>
-							)}
-						</div>
-						<div className="header__secondary-info">
-							<ul className="header__genres-list">
-								{genres.map((genre, index) => {
-									return (
-										<li className="header__genre-tag" key={index}>
-											{genre}
-										</li>
-									);
-								})}
-							</ul>
-						</div>
-					</div>
-				</div>
 
-				{summary?.length > 0 && (
-					<div className="content__details-summary">
-						<strong>Sinopse:</strong>
-						<div
-							ref={seriesSummarry}
-							className={`content__summary ${
-								showingMore ? "content__summary--show-full" : ""
-							}`}
-						>
-							{summary.map((paragraph, i) => (
-								<p key={i}>{paragraph}</p>
-							))}
-						</div>
-						{!showingMore && (
-							<button
-								className="show-more__button"
-								aria-label="Mostrar mais"
-								onClick={() => setShowingMore(true)}
-							>
-								Mostrar mais
-							</button>
-						)}
-					</div>
-				)}
-			</div>
-			<ContentNavbar links={navLinks} />
-		</header>
-	) : (
-		<SkeletonHeader></SkeletonHeader>
+	return (
+		<ContentHeader
+			data={seriesInfo}
+			imageFilename={seriesCover}
+			backgroundImageUrl={`${
+				import.meta.env.REACT_APP_HOST_ORIGIN
+			}/images/medium/${seriesCover}`}
+			title={title}
+			authors={authors}
+			genres={genres}
+			isAdult={isAdult}
+			summary={summary}
+			actions={{ mainAction, dropdownOptions, isDisabled: !user }}
+			navLinks={navLinks}
+		/>
 	);
 }
