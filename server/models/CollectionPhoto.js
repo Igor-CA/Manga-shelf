1572bbf78db1c600885912a5fe6b24d9341e19a8
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-01-21 10:58:55 -0300

    added timestamp attribute to collectionPhotoSchema

--- server/models/CollectionPhoto.js
+++ server/models/CollectionPhoto.js
@@ -1,43 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CollectionPhotoSchema = new Schema({
-	user: { 
-		type: mongoose.Schema.Types.ObjectId, 
-		ref: "User", 
-		required: true 
+const CollectionPhotoSchema = new Schema(
+	{
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
+		imageUrl: {
+			type: String,
+			required: true,
+		},
+		description: {
+			type: String,
+			default: "",
+		},
+		date: {
+			type: Date,
+			required: true,
+		},
+		order: {
+			type: Number,
+			default: 0,
+		},
+		isVisible: {
+			type: Boolean,
+			default: true,
+		},
+		isAdultContent: {
+			type: Boolean,
+			default: false,
+		},
 	},
-	imageUrl: { 
-		type: String, 
-		required: true 
-	},
-	description: { 
-		type: String, 
-		default: "" 
-	},
-	date: { 
-		type: Date, 
-		required: true 
-	},
-	order: { 
-		type: Number, 
-		default: 0 
-	},
-	isVisible: { 
-		type: Boolean, 
-		default: true 
-	},
-	isAdultContent: {
-		type: Boolean,
-		default: false
-	},
-	createdAt: { 
-		type: Date, 
-		default: Date.now 
-	},
-});
+	{ timestamps: true },
+);
 
-// Index for efficient queries
 CollectionPhotoSchema.index({ user: 1, date: -1, order: 1 });
 
 module.exports = mongoose.model("CollectionPhoto", CollectionPhotoSchema);
