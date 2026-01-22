fadf4c8775ff211cb99347d45653f9c275a23ad0
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-01-22 10:42:52 -0300

    missing volume summary on volume page

--- client/src/pages/VolumePage/VolumesOverallPage.jsx
+++ client/src/pages/VolumePage/VolumesOverallPage.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useRef, useState } from "react";
 import { formatDate, printArray } from "../../utils/seriesDataFunctions";
 
 export default function VolumesOverallPage({ volume }) {
@@ -11,19 +11,22 @@ export default function VolumesOverallPage({ volume }) {
 		freebies,
 		chapters,
 		hasVariant,
+		summary,
 	} = volume;
+	const volumeSummarry = useRef(null);
+	const [showingMore, setShowingMore] = useState(false);
 	const detailsSchema = useMemo(() => {
 		if (!volume) return [];
 
 		return [
 			{ label: "Autores", value: printArray(serie.authors) },
 			{ label: "Número de páginas", value: pagesNumber },
-			{ label: "Data de lançamento", value:formatDate(date) },
+			{ label: "Data de lançamento", value: formatDate(date) },
 			{ label: "Preço de capa", value: defaultPrice },
 			{ label: "ISBN", value: ISBN },
 			{ label: "Capitulos", value: chapters },
 			{ label: "Possui Capas variantes", value: hasVariant },
-            /*
+			/*
             //Will uncomment after collecting a decent amount of data for it
 			{ label: "Possui brindes", value: freebies.lenght > 0 ? "Sim" : "Não" },
 			{
@@ -33,10 +36,33 @@ export default function VolumesOverallPage({ volume }) {
             */
 		];
 	}, [volume]);
-
 	return (
 		<div className="container">
 			<div className="content-overall__container">
+				{summary?.length > 0 && (
+					<div className="content__details-summary content__details-summary--mobile">
+						<strong>Sinopse:</strong>
+						<div
+							ref={volumeSummarry}
+							className={`content__summary ${
+								showingMore ? "content__summary--show-full" : ""
+							}`}
+						>
+							{summary.map((paragraph, i) => (
+								<p key={i}>{paragraph}</p>
+							))}
+						</div>
+						{!showingMore && (
+							<button
+								className="show-more__button"
+								aria-label="Mostrar mais"
+								onClick={() => setShowingMore(true)}
+							>
+								Mostrar mais
+							</button>
+						)}
+					</div>
+				)}
 				<ul className="all-info-container">
 					{detailsSchema.map((item, index) => {
 						if (
