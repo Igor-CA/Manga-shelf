21a75b8a272ebfb647e9bb333195064f0c3a9dda
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-01-24 19:05:30 -0300

    missing file last commit

---
+++ server/models/Submission.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const SubmissionSchema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+			index: true,
+		},
+
+		targetModel: {
+			type: String,
+			required: true,
+			enum: ["Series", "Volume"],
+		},
+
+		targetId: {
+			type: Schema.Types.ObjectId,
+			refPath: "targetModel",
+			index: true,
+		},
+
+		payload: {
+			type: Schema.Types.Mixed,
+			required: true,
+		},
+
+		notes: {
+			type: String,
+			required: true,
+		},
+
+		status: {
+			type: String,
+			enum: ["Pendente", "Aprovado", "Rejeitado"],
+			default: "Pendente",
+			index: true,
+		},
+
+		adminComment: {
+			type: String,
+		},
+
+		reviewedBy: {
+			type: Schema.Types.ObjectId,
+			ref: "User",
+		},
+	},
+	{ timestamps: true },
+);
+
+module.exports = mongoose.model("Submission", SubmissionSchema);
