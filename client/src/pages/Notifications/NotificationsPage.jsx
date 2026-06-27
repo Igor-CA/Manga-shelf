f18e588e65de2e41bb8a7dce7e57e6e7226dfc7c
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-06-26 23:50:11 -0300

    refactor notification to use richText component

--- client/src/pages/Notifications/NotificationsPage.jsx
+++ client/src/pages/Notifications/NotificationsPage.jsx
@@ -3,6 +3,7 @@ import { UserContext } from "../../contexts/userProvider";
 import { Link, useNavigate } from "react-router-dom";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 import SideNavbar from "../../components/navbars/SideNavbar";
+import RichText from "../../components/RichText";
 import axios from "axios";
 import "./NotificationsPage.css";
 
@@ -45,45 +46,6 @@ const fetchNotifications = async (page, group) => {
 	}
 };
 
-const RichText = ({ text }) => {
-	if (!text) return null;
-
-	const regex = /(\[\[.*?\|.*?\]\])|(\*\*.*?\*\*)|(@\w+)/g;
-
-	return (
-		<>
-			{text
-				.split(regex)
-				.filter(Boolean)
-				.map((part, index) => {
-					if (part.startsWith("[[") && part.endsWith("]]")) {
-						const content = part.slice(2, -2);
-						const [label, url] = content.split("|");
-						return (
-							<Link key={index} to={url} className="notification-link">
-								{label}
-							</Link>
-						);
-					}
-
-					if (part.startsWith("**") && part.endsWith("**")) {
-						return <strong key={index}>{part.slice(2, -2)}</strong>;
-					}
-
-					if (part.startsWith("@")) {
-						const username = part.slice(1);
-						return (
-							<Link key={index} to={`/user/${username}`} className="mention">
-								{part}
-							</Link>
-						);
-					}
-
-					return <span key={index}>{part}</span>;
-				})}
-		</>
-	);
-};
 export default function NotificationsPage() {
 	const { user, isFetching } = useContext(UserContext);
 	const navigate = useNavigate();
