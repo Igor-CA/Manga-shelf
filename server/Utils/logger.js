d1a166a2badd2c5ecf1c1f42ea3981e2921eb548
Igor-CA <igorcaldeira.andrade@gmail.com>
2026-07-27 12:23:26 -0300

    added timestamps to logger

--- server/Utils/logger.js
+++ server/Utils/logger.js
@@ -32,12 +32,18 @@ const transports = [
 	new winston.transports.File({
 		filename: "logs/error.log",
 		level: "error",
-		format: winston.format.json(),
+		format: winston.format.combine(
+			winston.format.timestamp(),
+			winston.format.json(),
+		),
 	}),
 
 	new winston.transports.File({
 		filename: "logs/combined.log",
-		format: winston.format.json(),
+		format: winston.format.combine(
+			winston.format.timestamp(),
+			winston.format.json(),
+		),
 	}),
 ];
 
